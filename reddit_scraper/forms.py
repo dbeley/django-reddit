d3ece9ea13e271ca26267e849e321e607427a8a1
dbeley <6568955+dbeley@users.noreply.github.com>
2020-03-18 10:51:18 +0100

    Change max_length of forms fields.

--- reddit_scraper/forms.py
+++ reddit_scraper/forms.py
@@ -3,17 +3,17 @@ from django import forms
 
 class PostComments(forms.Form):
     post_urls = forms.CharField(
-        label="Reddit posts urls (separated by comma)", max_length=200
+        label="Reddit posts urls (separated by comma)", max_length=5000
     )
 
 
 class UserComments(forms.Form):
     username = forms.CharField(
-        label="Reddit usernames (separated by comma)", max_length=200
+        label="Reddit usernames (separated by comma)", max_length=500
     )
 
 
 class UserPosts(forms.Form):
     username = forms.CharField(
-        label="Reddit usernames (separated by comma)", max_length=200
+        label="Reddit usernames (separated by comma)", max_length=500
     )
